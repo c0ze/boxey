15dd7429f96c4b0b07961c4c03505498b73c7446
Arda Karaduman <akaraduman@gmail.com>
2015-01-24 13:17:50 +0900

    animations

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -15,6 +15,7 @@ class AppDelegate
     @blue_view = UIView.alloc.initWithFrame(CGRect.new([10, 40], [100, 100]))
     @blue_view.backgroundColor = UIColor.blueColor
     @window.addSubview(@blue_view)
+
     @add_button = UIButton.buttonWithType(UIButtonTypeSystem)
     @add_button.setTitle("Add", forState:UIControlStateNormal)
     @add_button.sizeToFit
@@ -22,19 +23,62 @@ class AppDelegate
       [10, @window.frame.size.height - 10 - @add_button.frame.size.height],
       @add_button.frame.size)
     @window.addSubview(@add_button)
+
     @add_button.addTarget(
       self, action:"add_tapped", forControlEvents:UIControlEventTouchUpInside)
+    @remove_button = UIButton.buttonWithType(UIButtonTypeSystem)
+    @remove_button.setTitle("Remove", forState:UIControlStateNormal)
+    @remove_button.sizeToFit
+    @remove_button.frame = CGRect.new(
+      [@add_button.frame.origin.x + @add_button.frame.size.width + 10,
+        @add_button.frame.origin.y],
+      @remove_button.frame.size)
+    @window.addSubview(@remove_button)
+    @remove_button.addTarget(
+      self, action:"remove_tapped",
+      forControlEvents:UIControlEventTouchUpInside)
     true
   end
 
   def add_tapped
     new_view = UIView.alloc.initWithFrame(CGRect.new([0, 0], [100, 100]))
     new_view.backgroundColor = UIColor.blueColor
+
     last_view = @window.subviews[0]
     new_view.frame = CGRect.new(
       [last_view.frame.origin.x,
         last_view.frame.origin.y + last_view.frame.size.height + 10],
       last_view.frame.size)
     @window.insertSubview(new_view, atIndex:0)
+  end
+
+  def remove_tapped
+    other_views = @window.subviews.reject { |view|
+      view.is_a?(UIButton)
+    }
+    last_view = other_views.last
+    return unless last_view && other_views.count > 1
+
+    animations_block = lambda {
+      last_view.alpha = 0
+      last_view.backgroundColor = UIColor.redColor
+      other_views.reject { |view|
+        view == last_view
+      }.each { |view|
+        new_origin = [
+          view.frame.origin.x,
+          view.frame.origin.y - (last_view.frame.size.height + 10)
+        ]
+        view.frame = CGRect.new(
+          new_origin,
+          view.frame.size)
+      }
+    }
+    completion_block = lambda { |finished|
+      last_view.removeFromSuperview
+    }
+    UIView.animateWithDuration(0.5,
+      animations: animations_block,
+      completion: completion_block)
   end
 end
