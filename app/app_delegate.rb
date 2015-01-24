c6015fde75887495da1900684cf6e0757b4f48c3
Arda Karaduman <akaraduman@gmail.com>
2015-01-24 13:16:46 +0900

    add buttons

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,9 +1,9 @@
 #---
 # Excerpted from "RubyMotion",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
+# We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/carubym for more book information.
 #---
 class AppDelegate
@@ -15,7 +15,26 @@ class AppDelegate
     @blue_view = UIView.alloc.initWithFrame(CGRect.new([10, 40], [100, 100]))
     @blue_view.backgroundColor = UIColor.blueColor
     @window.addSubview(@blue_view)
-
+    @add_button = UIButton.buttonWithType(UIButtonTypeSystem)
+    @add_button.setTitle("Add", forState:UIControlStateNormal)
+    @add_button.sizeToFit
+    @add_button.frame = CGRect.new(
+      [10, @window.frame.size.height - 10 - @add_button.frame.size.height],
+      @add_button.frame.size)
+    @window.addSubview(@add_button)
+    @add_button.addTarget(
+      self, action:"add_tapped", forControlEvents:UIControlEventTouchUpInside)
     true
+  end
+
+  def add_tapped
+    new_view = UIView.alloc.initWithFrame(CGRect.new([0, 0], [100, 100]))
+    new_view.backgroundColor = UIColor.blueColor
+    last_view = @window.subviews[0]
+    new_view.frame = CGRect.new(
+      [last_view.frame.origin.x,
+        last_view.frame.origin.y + last_view.frame.size.height + 10],
+      last_view.frame.size)
+    @window.insertSubview(new_view, atIndex:0)
   end
 end
