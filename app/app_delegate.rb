6d6b11a4924af9d40b77191f45ea80f173374c71
Arda Karaduman <akaraduman@gmail.com>
2015-01-24 13:15:16 +0900

    initial commit

---
+++ app/app_delegate.rb
@@ -0,0 +1,21 @@
+#---
+# Excerpted from "RubyMotion",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/carubym for more book information.
+#---
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.backgroundColor = UIColor.whiteColor
+    @window.makeKeyAndVisible
+
+    @blue_view = UIView.alloc.initWithFrame(CGRect.new([10, 40], [100, 100]))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@blue_view)
+
+    true
+  end
+end
